73506eb6d0c2417a2146223330eb36b529e7410f
Daniel Füvesi <daniel.fuevesi@gmail.com>
2018-09-08 12:23:17 +0200

    clickbait-update: implement ClickBaitVoteService tests

---
+++ clickbait-service-update/src/test/kotlin/com/clickbait/defeater/clickbaitservice/update/config/DbIntegrationTest.kt
@@ -0,0 +1,12 @@
+package com.clickbait.defeater.clickbaitservice.update.config
+
+/**
+ * <h4>About this class</h4>
+ *
+ * <p>Description</p>
+ *
+ * @author Daniel Fuevesi
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+interface DbIntegrationTest
