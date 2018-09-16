531cbd02f8f977a7256246bf15b83a16d25a7908
Daniel Füvesi <daniel.fuevesi@gmail.com>
2018-09-16 12:12:59 +0200

    extension: update gateway port from 8081 to 10487

--- chrome-extension/config.js
+++ chrome-extension/config.js
@@ -1,5 +1,5 @@
 const hostDomain = "37.221.195.13";
-const hostPort = "8081";
+const hostPort = "10487";
 const hostProtocol = "http";
 const hostUrl = hostProtocol + "://" + hostDomain + ":" + hostPort;
 
